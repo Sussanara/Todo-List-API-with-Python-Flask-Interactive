0b36fbf2a5945f66c436b36b762f056029dfbc78
Sussanara <sussanaracely@gmail.com>
2022-07-28 22:34:52 +0000

    nuevo avance

--- src/app.py
+++ src/app.py
@@ -4,28 +4,20 @@ app = Flask(__name__)
 
 todos = [
     { "label": "My first task", "done": False },
-    { "label": "My second task", "done": False }
 ]
-@app.route('/')
-def nothing():
-    return 'nothing here, proceed to /todos'
 
 @app.route('/todos', methods=['GET'])
 def hello_world():
-    
-    json_object = jsonify(todos)
-    
-    return json_object
+    json_text = jsonify(todos)
+    return json_text
 
 @app.route('/todos', methods=['POST'])
 def add_new_todo():
     request_body = request.data
-    to_append = json.loads(request_body)
+    decoded_object = json.loads(request_body)
     print("Incoming request with the following body", request_body)
-    todos.append(to_append)
+    todos.append(decoded_object)
     return jsonify(todos)
-
-
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
